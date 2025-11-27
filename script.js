589771b977149134daeeec98bd1b812208680862
Realrudrapro <rudradg@proton.me>
2025-11-27 09:21:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function n1(){
+    alert("The Hong Kong airport reclamation project is a massive project envolving 367 million m^3. It combines two Islands, Chek Lap Kok and Lam Chau. It uses advanced techniques such as deep cement mixing. The organization responsible for this project was the Hong Kong goverment. ")
+
+}
+
+function n2(){
+    alert("Chek Lap Kok seems like any other airport however this airport is larger then an average airport. Notably the beautiful work, this includes pieces such as \" A Dew Drop \" by Danny Lee Chin Fai. Don't forget the beautiful digital work. However land reclamation can be harmful to the environment by killing marine ecosystems because it can kill coral reefs which are life, also a type land reclaimation known as wetland reclaimation can significantly impact life because it is a process that removes water which many species need including land species for drinking and hunting fish.  ")
+
+}
+
+ function n3(){
+    alert("You may ask whats economic impact did the country face? ")
+ }
